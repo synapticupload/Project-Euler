9621b9e84d62b5aaf1e2cfc16249661f38ec3c61
synapticupload <synapticupload@gmail.com>
2019-10-27 20:24:10 -0500

    added problem 9 comments

--- 9.0.c
+++ 9.0.c
@@ -10,27 +10,30 @@
 // Find the product abc.
 // 
 
-// 
-
 #include <stdio.h>
-#include <math.h> 
-
-// pow(double x, double y) 
-// sqrt(double x)
+#include <math.h> // for pow(double x, double y) and sqrt(double x)
 
 int main(void) {
     
     double c = 0;
 
+    // use incrementing values for a and b
     for (double a = 0; a < 1000; a++) {
+
+        // set up b to be a, so b will always be greater than a anyways
         for (double b = a; b < 1000; b++) {
+
+            // obtain length of hypotenuse using math.h
             c = sqrt(pow(a, 2) + pow(b, 2));
+
+            // if total = 1000, and ensuring that a < b < c...
             if (((a + b + c) == 1000) && a < b && b < c) {
+
+                // print the results
                 printf("a = %.2lf, b = %.2lf, c = %.2lf\n", a, b, c);
                 printf("product = %.2lf\n", a * b * c);
             }
         }
     }
-        
     return 0;
 }
