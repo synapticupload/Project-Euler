1d94062e7e318c055d43849a209d558f981d7f4e
synapticupload <synapticupload@gmail.com>
2020-05-18 10:02:05 -0500

    test

--- 17.0.py
+++ 17.0.py
@@ -1,9 +1,24 @@
+# ------------------------------------------------------------------------------
+# If the numbers 1 to 5 are written out in words: 
+#     one, two, three, four, five... 
+#     ... then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+# 
+# If all the numbers from 1 to 1000 (one thousand) inclusive,
+#     were written out in words, how many letters would be used?
+# 
+# NOTE: Do not count spaces or hyphens. For example: 
+#     342 (three hundred and forty-two) contains 23 letters 
+#     115 (one hundred and fifteen) contains 20 letters. 
+# 
+#     The use of "and" when writing numbers is in compliance w/British usage.
+# ------------------------------------------------------------------------------
+
+
 ones_words =    [   'one', 'two', 'three', 'four', 'five', 
                     'six', 'seven', 'eight', 'nine']
 teens_words =   [   'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
                     'sixteen', 'seventeen', 'eighteen', 'nineteen'  ]
 tens_words =    [   'ten', 'twenty', 'thirty', 'fourty', 'fifty',
-                    'sixty', 'seventy', 'eighty', 'ninety']
 
 def num2words(x):
     string = ''
